0990aedee63943b6a6de9c89615bd0baf1b851dc
Lorenz Rychener <lorenz.rychener@hefr.ch>
2020-06-17 15:29:02 +0200

    normalizations for dataset

--- src/timeatlas/models/NN/dataloader/base_loader.py
+++ src/timeatlas/models/NN/dataloader/base_loader.py
@@ -1,6 +1,8 @@
 from torch.utils.data import Dataset
 
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import normalize
+
+import numpy as np
 
 
 class BaseDataset(Dataset):
@@ -8,12 +10,12 @@ class BaseDataset(Dataset):
     def __init__(self, data=None):
         self.data = data
 
-    def min_max_norm(self):
-        scaler = MinMaxScaler()
-        return scaler.fit_transform(self.data.T).T
+    def min_max_norm(self, min: int = 0, max: int = 1):
+        xmax, xmin = self.data.max(), self.data.min()
+        data_norm = (self.data - xmin) / (xmax - xmin)
+        return data_norm
 
     def z_score_norm(self):
-        raise NotImplementedError
-
-    def sigmoid_norm(self):
-        raise NotImplementedError
+        mu, sigma = self.data.mean(), self.data.std()
+        data_norm = (self.data - mu) / sigma
+        return data_norm
