c14f44613c0db98bab042831d6b59551e84faaca
Lorenz Rychener <lorenz.rychener@hefr.ch>
2020-06-25 20:37:41 +0200

    removed unneeded scaler in datasets

--- src/timeatlas/models/NN/dataset/base_dataset.py
+++ src/timeatlas/models/NN/dataset/base_dataset.py
@@ -8,32 +8,5 @@ class BaseDataset(Dataset):
     Base Class for the PyTorch DataSets
     """
 
-    def __init__(self, data=None):
-        self.data = data
-
-        self.min = None
-        self.max = None
-        self.mu = None
-        self.sigma = None
-
-    def min_max_norm(self):
-        """
-
-        Normalization of the Dataset between 0 and 1
-
-        x_norm_ji = (x_ji - min) / (max -min)
-
-        """
-        self.max, self.min = self.data.max(), self.data.min()
-        self.data = (self.data - self.min) / (self.max - self.min)
-
-    def z_score_norm(self):
-        """
-
-        Normalization according to the standard scoring
-
-        x_norm_ij = (x_ji - mean) / std
-
-        """
-        self.mu, self.sigma = self.data.mean(), self.data.std()
-        self.data = (self.data - self.mu) / self.sigma
+    def __init__(self):
+        pass
