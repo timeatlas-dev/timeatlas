154fee78d8bef9c0376395d034de227d355965b4
Fred Montet <fredmontet@gmail.com>
2020-07-08 11:46:48 +0200

    improved requirements handling

--- setup.py
+++ setup.py
@@ -26,5 +26,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
-    install_requires=read_requirements('requirements.txt'),
+    install_requires=read_requirements('requirements/src.txt'),
 )
