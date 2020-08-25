50621771a782d4537db133a596019d5023e1692f
Fred Montet <fredmontet@gmail.com>
2020-08-25 16:53:42 +0200

    Update prediction and status plot

--- src/timeatlas/plots/time_series.py
+++ src/timeatlas/plots/time_series.py
@@ -1,5 +1,8 @@
 from typing import TYPE_CHECKING
+from datetime import datetime
 
+import numpy as np
+import seaborn as sns
 from matplotlib import pyplot as plt
 import matplotlib.dates as mdates
 from pandas.plotting import register_matplotlib_converters
@@ -43,54 +46,62 @@ def line(ts: 'TimeSeries', *args, **kwargs):
             ax.set_title("{}—{}".format(sensor.id, sensor.name))
 
 
-def prediction(ts: 'TimeSeries', pred: 'TimeSeries'):
+def prediction(forecast: 'TimeSeries', observation: 'TimeSeries' = None):
     """
-    Make a plot to display a chunk of a TimeSeries and a prediction with its
-    confidence interval
+    Make a plot to display a time series with forecasted values and its
+    confidence interval. If given, the observation time series can be added as
+    an optional argument.
 
     Args:
-        ts: TimeSeries of the historical data
-        pred: TimeSeries of the prediction
+        forecast: TimeSeries of the prediction
+        observation: TimeSeries of the historical data
     """
-    fig = plt.figure(figsize=(18,4))
+    fig = plt.figure(figsize=(18, 4))
     ax = fig.add_subplot(111)
     ax.grid(True, which='major', c='gray', ls='-', lw=1, alpha=0.2)
+    ax.set_xlabel("Date")
 
     # Add contextual information to the plot
-    if ts.metadata is not None:
-        if "sensor" in ts.metadata:
-            sensor = ts.metadata["sensor"]
-            duration = str(pred.duration())
+    if forecast.metadata is not None:
+        if "sensor" in forecast.metadata:
+            sensor = forecast.metadata["sensor"]
+            duration = str(forecast.duration())
             sensor_str = "{}—{}".format(sensor.id, sensor.name)
             ax.set_title("Forecast of {} for {}".format(sensor_str, duration))
         else:
-            ax.set_title("Forecast for {}".format(str(pred.duration())))
+            ax.set_title("Forecast for {}".format(str(forecast.duration())))
 
-        if "unit" in ts.metadata:
-            unit = ts.metadata["unit"]
+        if "unit" in forecast.metadata:
+            unit = forecast.metadata["unit"]
             ax.set_ylabel("{} $[{}]$".format(unit.name, unit.symbol))
 
-    ax.set_xlabel("Date")
-
     # Add the lines to the plot
-    ax.plot(ts.series.index, ts.series.values, ls='-', c="0.3",
-            label="Measurement")
-    ax.plot(pred.series.index, pred.series["values"].values, ls='--', c='k',
-            label="Prediction")
-    ax.fill_between(pred.series.index, pred.series["ci_lower"].values,
-                    pred.series["ci_upper"].values,
+    ax.plot(forecast.series.index, forecast.series["values"].values,
+            ls='--',
+            c='k',
+            label="prediction")
+    ax.fill_between(forecast.series.index, forecast.series["ci_lower"].values,
+                    forecast.series["ci_upper"].values,
                     color='0.86',
-                    label="Confidence Interval")
+                    label="confidence interval")
+
+    if observation is not None:
+        ax.plot(observation.series.index, observation.series.values,
+                ls='-',
+                c="0.3",
+                label="observation")
+
     ax.legend()
 
 
-def status(ts: 'TimeSeries'):
+def status(ts: 'TimeSeries', cmap: str = "autumn_r"):
     """
     Plot a uni-dimensional imshow to mimic status plots like on
     https://githubstatus.com
 
     Args:
         ts: TimeSeries - the time series to plot
+        cmap: String - the matplotlib colormap to use
     """
     register_matplotlib_converters()
 
@@ -107,13 +118,12 @@ def status(ts: 'TimeSeries'):
     ax.xaxis.set_major_formatter(date_format)
 
     ax.set_yticks([])  # remove all yticks
+    ax.xaxis_date()
 
     m = ax.imshow([ts.series],
               extent=[x_lims[0], x_lims[1],  y_lims[0], y_lims[1]],
               aspect='auto',
-              cmap="autumn_r")
-
-    ax.xaxis_date()
+              cmap=cmap)
 
     ax = add_metadata_to_plot(ts.metadata, ax)
 
