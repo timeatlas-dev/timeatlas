e158d8d93431ea41d6ee2b70ab9299e7c07005e3
Fred Montet <fredmontet@gmail.com>
2020-10-05 19:23:01 +0200

    Started the prophet with tsd prediction

--- src/timeatlas/models/prophet.py
+++ src/timeatlas/models/prophet.py
@@ -21,13 +21,28 @@ class Prophet(AbstractBaseModel):
         self.model = fbp.Prophet()
 
     def fit(self, ts: Union[TimeSeries, TimeSeriesDataset],
-            value: Optional = None) -> NoReturn:
+            y: Optional[int] = None) -> NoReturn:
+        """
+        Fit a Prophet model to a time series. If given a TimeSeriesDataset, the
+        optional argument y must be given to indicate on which component the
+        model should be fitted to.
+
+        Args:
+            ts: TimeSeries or TimeSeriesDataset to fit
+            y: Optional int of the component index in a TimeSeriesDataset
+
+        Returns:
+            NoReturn
+        """
         super().fit(ts)
 
         if isinstance(ts, TimeSeries):
             df = self.__prepare_time_series_for_prophet(self.X_train)
         elif isinstance(ts, TimeSeriesDataset):
-            df = self.__prepare_time_series_dataset_for_prophet(self.X_train)
+            df = self.__prepare_time_series_dataset_for_prophet(self.X_train, y)
+
+            # TODO Continue here by adding the add_regressors() in a loop!
+
         else:
             ValueError('The fit method  accepts only TimeSeries or '
                        'TimeSeriesDataset as argument')
@@ -71,8 +86,13 @@ class Prophet(AbstractBaseModel):
         return df
 
     @staticmethod
-    def __prepare_time_series_dataset_for_prophet(tsd: TimeSeriesDataset):
-        df = tsd.to_df()
+    def __prepare_time_series_dataset_for_prophet(tsd: TimeSeriesDataset,
+                                                  y: int):
+        df = tsd.to_df().copy()
+        df["ds"] = df.index
+        df = df.reset_index(drop=True)
+        df = df.rename(columns={y: "y"})
+        return df
 
     def make_future_dataframe(self, horizon: str, freq: str = None):
         index = self.make_future_index(horizon, freq)
